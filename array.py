78566f3605de117013e5b4eaad8236e220ded780
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2025-11-27 17:08:01 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,16 +1,12 @@
 import sys
-
 if len(sys.argv) < 2:
     print("Usage: python array_scores.py <score1> <score2> <score3> ...")
     sys.exit()
-
 scores = list(map(int, sys.argv[1:]))
-
 total = sum(scores)
 average = total / len(scores)
 maximum = max(scores)
 minimum = min(scores)
-
 print("Scores:", scores)
 print("Sum =", total)
 print("Average =", average)
